d4c4bc52a213e6679d52c0b94f7622651bf3f9ca
yezid00 <olanaseyezid@gmail.com>
2020-06-04 00:23:43 +0100

    set up the crud function

---
+++ app/Http/Controllers/CompanyController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Company;
+class CompanyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $companies = Company::orderBy('name','asc')->paginate(5);
+        return view('companies.index')->withCompanies($companies); 
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('companies.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'name'=>'required|min:4|max:255',
+            'email'=>'required|email',
+            'address'=>'required|max:255',
+            'website'=>'required',
+        ]);
+        $company = new Company;
+        $company->name = $request->name;
+        $company->email = $request->email;
+        $company->address = $request->address;
+        $company->website = $request->website;
+
+        $company->save();
+        return redirect()->route('home')->with('success','New Company Added');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $company = Company::find($id);
+        return view('companies.edit')->with('company',$company);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+
+        $this->validate($request, [
+            'name'=>'required|min:4|max:255',
+            'email'=>'required|email',
+            'address'=>'required|max:255',
+            'website'=>'required',
+        ]);
+        $company = Company::find($id);
+        $company->name = $request->name;
+        $company->email = $request->email;
+        $company->address = $request->address;
+        $company->website = $request->website;
+
+        $company->save();
+        return redirect()->route('companies.index')->with('success',$company->name. ' details has been updated'); 
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $company = Company::find($id);
+        $company->delete();
+
+        return redirect()->route('companies.index')->with('success','Company deleted');
+    }
+}
