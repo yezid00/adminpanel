7691314ab1dda2bbba2a5cb540f2281bf5a92284
yezid00 <olanaseyezid@gmail.com>
2020-06-04 03:58:15 +0100

    added dropdown items

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -33,6 +33,15 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('home') }}">
+                                       
+                                        {{ __('Dashboard') }}
+                                    </a>
+                                    <a class="dropdown-item" href="{{ route('employees.create') }}">
+                                       
+                                        {{ __('Add new Employee') }}
+                                    </a>
+
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
